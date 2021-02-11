b75b4a438d7ba98de67611679244b57e2c33e758
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2021-02-11 23:27:20 +0600

    Add rang & design title with colors
    Rang is a 3rd party library for stling and colorizing the termianl output

--- include/ffmpeg-coder/utils.hpp
+++ include/ffmpeg-coder/utils.hpp
@@ -1,7 +1,9 @@
 #if !defined(UTILS_H)
 #define UTILS_H
 
+#include "rang.hpp"
 #include <iostream>
+
 
 //A function for clearing screen (Cross Platform)
 void clear_screen()
@@ -17,18 +19,23 @@ void clear_screen()
 
 // Function for dynamically decorating a text
 void decorateMe(std::string textToDecor){
+    using namespace rang;
     int chars = textToDecor.capacity();
     int counter = 0;
     std::cout<<std::endl;
+    std::cout<<fg::red;
     for (int i =0 ; i<(chars*2); i++){
         std::cout<<"_";
     }
+    std::cout<<fg::reset;
     std::cout<<std::endl;
-    std::cout<<textToDecor;
+    std::cout<<fg::blue<<style::bold<<textToDecor<<style::reset<<fg::reset;
     std::cout<<std::endl;
+    std::cout<<fg::red;
     for (int i =0 ; i<(chars*2); i++){
         std::cout<<"_";
     }
+    std::cout<<fg::reset;
     std::cout<<std::endl;
     std::cout<<std::endl;
     
--- src/ffmpeg-coder.cpp
+++ src/ffmpeg-coder.cpp
@@ -1,6 +1,6 @@
 /* A C++ program code by Mushfiqur Rahman Abir  */
 
-
+#include "rang.hpp"
 #include "ffmpeg_coder.hpp"
 #include "frontend_funcs.hpp"
 int main()
