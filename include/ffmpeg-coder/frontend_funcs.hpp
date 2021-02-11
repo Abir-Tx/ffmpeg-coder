117630220c1169a44529467f664f1060375d6c30
Abir-Tx <28858998+Abir-Tx@users.noreply.github.com>
2021-02-12 00:16:16 +0600

    Add navigating to the main screen from anywhere
    The navigation system of the app was broken. From now user can go back to the main screen from any module
    
    To achieve this a while loop has been used

--- include/ffmpeg-coder/frontend_funcs.hpp
+++ include/ffmpeg-coder/frontend_funcs.hpp
@@ -12,15 +12,15 @@ void title()
     decorateMe("FFMPEG Coder");
 }
 
-
 void about()
 {
     clear_screen();
     std::string about_ffmpeg_coder;
     std::ifstream read_about("res/about.bin");
 
-    while (getline(read_about, about_ffmpeg_coder)){
-        std::cout<<about_ffmpeg_coder;
+    while (getline(read_about, about_ffmpeg_coder))
+    {
+        std::cout << about_ffmpeg_coder;
     }
 }
 
@@ -31,47 +31,56 @@ void homepage()
 
     ffmpeg ffmpeg; //Creating ffmpeg object
     int choice;
-
-    cout << "Choose your option: " << endl;
-
-    //Adding the options to the homepage menu (Main menu)
-    vector<string> options;
-    options.push_back("Make Codes");
-    options.push_back("About");
-    options.push_back("Exit");
-
-    //showing the options
-    for (int i = 0; i < options.size(); i++)
+    
+    /* The char goBack variable is used for navigating to the main screen using the while loop */
+    char goBack;
+    do
     {
-        cout << (i + 1) << ". " << options[i] << endl;
-    }
+        cout << "Choose your option: " << endl;
 
-    //taking choice input
-    cout << "Your choice: ";
-    cin >> choice;
+        //Adding the options to the homepage menu (Main menu)
+        vector<string> options;
+        options.push_back("Make Codes");
+        options.push_back("About");
+        options.push_back("Exit");
 
-    //Entering into selected option funcs
-    if (choice == 1)
-    {
-        clear_screen();
-        ffmpeg.load_encodes();
-        ffmpeg.show_encodes();
-        ffmpeg.select_encodes();
-        ffmpeg.selected_action();
-    }
-    else if (choice == 2)
-    {
-        about();
-    }
-    else if (choice == 3)
-    {
-        std::cout << "Exiting the program..............." << std::endl;
-        exit(0);
-    }
-    else
-    {
-        exit(0);
-    }
+        //showing the options
+        for (int i = 0; i < options.size(); i++)
+        {
+            cout << (i + 1) << ". " << options[i] << endl;
+        }
+
+        //taking choice input
+        cout << "Your choice: ";
+        cin >> choice;
+
+        //Entering into selected option funcs
+        if (choice == 1)
+        {
+            clear_screen();
+            ffmpeg.load_encodes();
+            ffmpeg.show_encodes();
+            ffmpeg.select_encodes();
+            ffmpeg.selected_action();
+        }
+        else if (choice == 2)
+        {
+            about();
+        }
+        else if (choice == 3)
+        {
+            clear_screen();
+            std::cout << "Exiting the program..............." << std::endl;
+            exit(0);
+        }
+        else
+        {
+            exit(0);
+        }
+
+        std::cout << "Do you want to go to Home screen[Y/N]: ";
+        std::cin >> goBack;
+    } while (goBack == 'Y');
 }
 
 #endif // FRONTEND_FUNC_H
