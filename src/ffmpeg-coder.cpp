b531fe0888d59e381f82ccc45aa4c6e35a256365
Mushfiqur Rahman Abir <28858998+Abir-Tx@users.noreply.github.com>
2021-02-21 02:33:32 +0600

    Add clang format guideline

--- src/ffmpeg-coder.cpp
+++ src/ffmpeg-coder.cpp
@@ -1,34 +1,25 @@
 /* A C++ program code by Mushfiqur Rahman Abir  */
 
-#include "rang.hpp"
 #include "ffmpeg_coder.hpp"
 #include "frontend_funcs.hpp"
+#include "rang.hpp"
 #include "s_lock.hpp"
-int main()
-{
+int main() {
 #ifdef __unix__
     clear_screen();
     homepage();
 #else
     S_Lock slock;
-    if (slock.getIsLock() == true)
-    {
-        if (slock.unlocker() == true)
-        {
+    if (slock.getIsLock() == true) {
+        if (slock.unlocker() == true) {
             clear_screen();
             homepage();
-        }
-        else if (slock.unlocker() == false)
-        {
+        } else if (slock.unlocker() == false) {
             std::cout << "Exiting the app";
-        }
-        else
-        {
+        } else {
             exit(0);
         }
-    }
-    else
-    {
+    } else {
         clear_screen();
         homepage();
     }
